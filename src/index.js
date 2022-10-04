7ecf9468fef740bb702b173811c819e526d41bd3
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-10-04 13:13:29 +0900

    password generator

---
+++ src/index.js
@@ -0,0 +1,114 @@
+let leftBoxText = document.querySelector("#leftBoxText");
+let rightBoxText = document.querySelector("#rightBoxText");
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+
+function generatePassword() {
+  let passwordLeft = "";
+  let passwordRight = "";
+
+  for (let i = 0; i < passwordLength; i++) {
+    randomLeft = Math.floor(Math.random() * characters.length);
+    randomRight = Math.floor(Math.random() * characters.length);
+    passwordLeft += characters[randomLeft];
+    passwordRight += characters[randomRight];
+  }
+
+  leftBoxText.textContent = passwordLeft;
+  rightBoxText.textContent = passwordRight;
+}
+
+
